ea65217f5f597542724f7be50c203f78621dea13
Junyoung McArdle <juny.mcardle@gmail.com>
2018-11-26 07:02:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.push(name)} in line.`
 }
 
-// function nowServing(katzDeliLine) {
-//   if (katzDeliLine.length === 0) {
-//     return "There is nobody waiting to be served!"
-//   } else {
-//     return `Currently serving ${katzDeliLine.shift()}.`
-//   }
-// }
-//
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+}
+
 // function currentLine(katzDeliLine, name) {
 //   if (katzDeliLine.length === 0) {
 //     return "The line is currently empty."
