018c04cc51407c2e4fa8602fbe78077a6ed039a0
Junyoung McArdle <juny.mcardle@gmail.com>
2018-11-26 06:54:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ function nowServing(katzDeliLine) {
     return `Currently serving ${katzDeliLine.shift()}.`
   }
 }
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+     var line = []
+      for (let i = 0; i < katzDeliLine.length; i++)
+       line.push(`${i + 1}. ${katzDeliLine[i]}`
+  } return `The lie is currently: ${katzDeliLine[i], i++}`
+}
