387745e5a35467ad15e3cf88ac5b9bc9096e3d4c
Junyoung McArdle <juny.mcardle@gmail.com>
2018-11-26 07:01:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.push(name)} in line.`
 }
 
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
-    return "There is nobody waiting to be served!"
-  } else {
-    return `Currently serving ${katzDeliLine.shift()}.`
-  }
-}
-
-function currentLine(katzDeliLine, name) {
-  if (katzDeliLine.length === 0) {
-    return "The line is currently empty."
-  } else {
-     var line = []
-      for (let i = 0; i < katzDeliLine.length; i++)
-       line.push(`${i + 1}. ${line[i]}`
-  } return `The line is currently: ${line.join(', ')}`
-}
+// function nowServing(katzDeliLine) {
+//   if (katzDeliLine.length === 0) {
+//     return "There is nobody waiting to be served!"
+//   } else {
+//     return `Currently serving ${katzDeliLine.shift()}.`
+//   }
+// }
+//
+// function currentLine(katzDeliLine, name) {
+//   if (katzDeliLine.length === 0) {
+//     return "The line is currently empty."
+//   } else {
+//      var line = []
+//       for (let i = 0; i < katzDeliLine.length; i++)
+//        line.push(`${i + 1}. ${line[i]}`
+//   } return `The line is currently: ${line.join(', ')}`
+// }
