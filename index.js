8948ebda2e0172f496091575e20c276138f6fc99
Junyoung McArdle <juny.mcardle@gmail.com>
2018-11-26 06:56:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing(katzDeliLine) {
   }
 }
 
-function currentLine(katzDeliLine) {
+function currentLine(line) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
